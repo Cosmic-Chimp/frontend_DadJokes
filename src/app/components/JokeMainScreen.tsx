603bf64cd6368dc57450e786489db87a489db6cd
rueben-consnetcoz <rubzdebruyn@gmail.com>
2024-11-22 11:40:56 +0200

    will get dev errs now but its because the backedn isnt set up, network err on axios on api fetch

---
+++ src/app/components/JokeMainScreen.tsx
@@ -0,0 +1,49 @@
+"use client";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+function JokeMainScreen() {
+    const [jokes, setJokes] = useState([]);
+    const [randomJoke, setRandomJoke] = useState("");
+
+    // Fetch on component mount
+    useEffect(() => {
+        axios
+            .get("http://localhost:5000/api/jokes")
+            .then((response) => {
+                setJokes(response.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching jokes", error);
+            });
+    }, []);
+
+    // random joke get
+    const fetchRandomJoke = () => {
+        axios
+            .get("http://localhost:5000/api/jokes/random")
+            .then((response) => {
+                setRandomJoke(response.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching random joke", error);
+            });
+    };
+
+    return (
+        <div className="">
+            <h1>Dad Jokes</h1>
+            <button onClick={fetchRandomJoke}>Get a Random Joke</button>
+            <p>{randomJoke}</p>
+
+            <h2>All Jokes</h2>
+            <ul>
+                {jokes.map((joke, index) => (
+                    <li key={index}>{joke}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default JokeMainScreen;
